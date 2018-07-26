cbc95fa3cf101f3d3e3c0aace255a3809e73b280
unknown <Octavi@OCTAVI.VILA-SECA.INFO>
2018-07-26 17:54:24 +0200

    Products, catergories and log process

---
+++ account/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^register/$', views.register, name='register'),
+
+    # login / logout
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+
+    # canvi contrassenya
+    url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+
+    #restablir contrassenya
+    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+]
